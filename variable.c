b21061749ccaa75a29b918fae1908c10152e8b44
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2024-02-04 15:03:32 +0530

    Create variable.c

---
+++ variable.c
@@ -0,0 +1,9 @@
+// Create variables
+int myNum = 15;            // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
